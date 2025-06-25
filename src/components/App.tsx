990d7ccd702c1789e3485d50abfc5d3abe2574c9
WideSive <paco.bousquet@protonmail.com>
2025-06-25 11:16:03 +0200

    creation variable marge globale +lightStepLogo webp

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -2,6 +2,7 @@ import Band from "./Band";
 import HomeCategoriesPictures from "./HomeCategoriesPictures";
 import InfiniteBrandsSlider from "./InfiniteBrandsSlider";
 import NavBar from "./NavBar";
+import Footer from "./Footer";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <HomeCategoriesPictures />
         <InfiniteBrandsSlider />
       </div>
+      <Footer />
     </>
   );
 }
